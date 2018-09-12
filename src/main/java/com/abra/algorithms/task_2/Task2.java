c4fed9fa58441dd31ee338e47106bfb55e648591
Nikolai Bystrov <isderevni@gmail.com>
2018-09-12 09:59:47 +0300

    Task2 added sortArray method

--- src/main/java/com/abra/algorithms/task_2/Task2.java
+++ src/main/java/com/abra/algorithms/task_2/Task2.java
@@ -1,6 +1,5 @@
 package com.abra.algorithms.task_2;
 
-import java.util.Arrays;
 import org.apache.commons.lang.ArrayUtils;
 
 public class Task2 {
@@ -8,7 +7,6 @@ public class Task2 {
   public static void main(String[] args) {
     int[] mas = {7, 1, 2, 6, 4, 5, 3, 8};
 
-//    mergeFuntion(mas, 0, 4, 4, 8);
     mergeSort(mas, 0, 8);
 
     for (int i : mas) {
@@ -23,27 +21,55 @@ public class Task2 {
       mergeSort(mas, startInclusive, startInclusive + length / 2 + 1);
       mergeSort(mas, startInclusive + length / 2 + 1, endExclusive);
     }
-    
+
     mergeFuntion(mas, startInclusive, startInclusive + length / 2 + 1, startInclusive + length / 2 + 1, endExclusive);
   }
 
   private static void mergeFuntion(int[] mas, int leftStartIn, int leftEndEx, int rightStartIn, int rightEndEx) {
     int[] left = ArrayUtils.subarray(mas, leftStartIn, leftEndEx);
     int[] right = ArrayUtils.subarray(mas, rightStartIn, rightEndEx);
-    int[] result = ArrayUtils.addAll(left, right);
 
-    Arrays.sort(result);
+    int[] result = sortArray(left, right);
 
-    int[] leftOutside = ArrayUtils.subarray(mas, 0, leftStartIn);
-    int[] rightOutside = ArrayUtils.subarray(mas, rightEndEx, mas.length);
-
-    result = ArrayUtils.addAll(ArrayUtils.addAll(leftOutside, result), rightOutside);
-    copyToArray(mas, result);
+    copyToArray(mas, result, leftStartIn, rightEndEx);
   }
 
-  private static void copyToArray(int[] mas, int[] result) {
-    for (int i = 0; i < mas.length; i++) {
-      mas[i] = result[i];
+  private static int[] sortArray(int[] left, int[] right) {
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int resultIndex = 0;
+    int allElements = left.length + right.length;
+    int[] resultArray = new int[allElements];
+
+    while (resultIndex < allElements) {
+      if (leftIndex == left.length) {
+        for (; resultIndex < allElements; ) {
+          resultArray[resultIndex++] = right[rightIndex++];
+        }
+        break;
+      }
+
+      if (rightIndex == right.length) {
+        for (; resultIndex < allElements; ) {
+          resultArray[resultIndex++] = left[leftIndex++];
+        }
+        break;
+      }
+
+      if (right[rightIndex] <= left[leftIndex]) {
+        resultArray[resultIndex++] = right[rightIndex++];
+      } else {
+        resultArray[resultIndex++] = left[leftIndex++];
+      }
+    }
+
+    return resultArray;
+  }
+
+  private static void copyToArray(int[] mas, int[] result, int startInclusive, int endExclusive) {
+    int k = 0;
+    for (int i = startInclusive; i < endExclusive; i++) {
+      mas[i] = result[k++];
     }
   }
 }
