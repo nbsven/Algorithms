de8e91c5b128c90d92c61c9301c77f8b35c0e1a6
Nikolai Bystrov <isderevni@gmail.com>
2018-10-23 16:12:40 +0300

    Task6 contains, adding implemented

--- src/main/java/com/abra/algorithms/task_6/b_tree/Btree.java
+++ src/main/java/com/abra/algorithms/task_6/b_tree/Btree.java
@@ -1,5 +1,8 @@
 package com.abra.algorithms.task_6.b_tree;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Btree {
@@ -36,44 +39,117 @@ public class Btree {
   }
 
   public void add(int key) {
-    if (root.getChildren().isEmpty() && root.getElements().size() < 2 * factor - 2) {
-      addElementToList(key, root.getElements());
+    if (root.getChildren().isEmpty()) {
+
+      if (root.getElements().size() < 2 * factor - 1) {
+        addElementToList(key, root.getElements());
+      } else {
+        root = splitNode(root, null, -1);
+      }
+
+      return;
     }
 
-    add(key, root);
+    root = add(key, root);
   }
 
-  private int add(int key, Node node) {
-    List<Integer> elements = node.getElements();
+  private Node splitNode(Node child, Node parent, int index) {
+    Integer middle = child.getElements().get(factor - 1);
 
-    for (int i = 0; i < elements.size(); i++) {
-      Integer element = elements.get(i);
+    List<Integer> leftElements = child.getElements().subList(0, factor - 1);
+    List<Integer> rightElements = child.getElements().subList(factor, 2 * factor - 1);
 
+    List<Node> leftNodes = Collections.emptyList();
+    List<Node> rightNodes = Collections.emptyList();
+
+    if (!child.getChildren().isEmpty()) {
+      leftNodes = child.getChildren().subList(0, factor);
+      rightNodes = child.getChildren().subList(factor, 2 * factor);
+    }
+
+    Node left = new Node(leftElements, leftNodes);
+    Node right = new Node(rightElements, rightNodes);
+
+    if (parent == null) {
+      List<Integer> elements = new LinkedList<>();
+      elements.add(middle);
+
+      List<Node> nodes = new LinkedList<>();
+      nodes.add(left);
+      nodes.add(right);
+
+      return new Node(elements, nodes);
+    }
+
+    parent.getElements().add(index, middle);
+
+    parent.getChildren().add(index, left);
+    parent.getChildren().add(index + 1, right);
+
+    return parent;
+  }
+
+  private Node add(int key, Node node) {
+
+    int count = node.getElements().size();
+
+    Node ref = node;
+    for (int i = 0; i < count; i++) {
+      Integer element = ref.getElements().get(i);
+      if (key == element) {
+        return node;
+      }
       if (key < element) {
-        int temp = add(key, node.getChildren().get(i));
 
-        if (temp == THRESHOLD) {
-          return THRESHOLD;
-        }
+        if (ref.getChildren().isEmpty()) {
+          addElementToList(key, ref.getElements());
 
-        if (elements.size() < 2 * factor - 2) {
-          addElementToList(temp, elements);
+          return node;
         } else {
-          addElementToList(temp,elements);
+          Node child = ref.getChildren().get(i);
 
+          if (child.getElements().size() < 2 * factor - 1) {
+            ref.getChildren().set(i, add(key, child));
 
+            return ref;
+          } else {
+            ref = splitNode(child, ref, i);
+            count++;
+            i--;
+          }
         }
+      } else if (i == count - 1) {
+        if (ref.getChildren().isEmpty()) {
+          addElementToList(key, ref.getElements());
 
+          return node;
+        } else {
+          Node child = ref.getChildren().get(i+1);
+
+          if (child.getElements().size() < 2 * factor - 1) {
+            ref.getChildren().set(i, add(key, child));
+
+            return ref;
+          } else {
+            ref = splitNode(child, ref, i);
+            count++;
+            i--;
+          }
+        }
       }
     }
+
+    return ref;
   }
 
   private void addElementToList(int key, List<Integer> elements) {
     for (int i = 0; i < elements.size(); i++) {
       Integer element = elements.get(i);
 
-      if (key <= element) {
+      if (key < element) {
         elements.add(i, key);
+      } else if (key == element) {
+        return;
       }
     }
   }
--- src/main/java/com/abra/algorithms/task_6/b_tree/Node.java
+++ src/main/java/com/abra/algorithms/task_6/b_tree/Node.java
@@ -2,14 +2,15 @@ package com.abra.algorithms.task_6.b_tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Node {
 
-  private int factor;
   private List<Integer> elements = new ArrayList<>();
   private List<Node> children = new ArrayList<>();
 }
