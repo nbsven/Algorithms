b03f7af5f111d28669e1a0db18421b43a06421a5
Vadim Plaksin <plaksinvadimaleks@gmail.com>
2019-01-14 22:38:12 +0300

    Task8 - done as task9

--- src/main/java/com/abra/algorithms/task_8/Task8.java
+++ src/main/java/com/abra/algorithms/task_8/Task8.java
@@ -1,41 +1,114 @@
 package com.abra.algorithms.task_8;
 
-import com.abra.algorithms.task_7.MySet;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import lombok.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Scanner;
+
+@SuppressWarnings("all")
+//https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 public class Task8 {
 
-  public static void main(String[] args) throws IOException {
-    try (BufferedReader input = new BufferedReader(new FileReader("src/main/resources/Task7/input.txt"));
-        BufferedWriter output = new BufferedWriter(new FileWriter("src/main/resources/Task7/output.txt"))) {
-      Integer N = Integer.valueOf(input.readLine());
+    private static Vertex[] graph;
+    private static Integer verticesCount;
+    private static Integer edgesCount;
 
+    public static void main(String[] args) throws Exception {
+        readDataFromFile("src/main/resources/Task8/input.txt");
 
-//      for (int i = 0; i < N; i++) {
-//        String line = input.readLine();
-//
-//        String[] split = line.split("\\s");
-//        int X = Integer.parseInt(split[1]);
-//
-//        switch (split[0]) {
-//          case "?": {
-//            output.write(set.contains(X) + "\n");
-//            break;
-//          }
-//          case "-":
-//            set.remove(X);
-//            break;
-//          case "+":
-//            set.add(X);
-//            break;
-//        }
-//      }
+        graph[0].setDistance(0);
+
+        Vertex vertexWithMinDistance = findVertexWithMinDistance();
+
+        while (vertexWithMinDistance != null) {
+            relaxDistances(vertexWithMinDistance);
+
+            vertexWithMinDistance = findVertexWithMinDistance();
+        }
+
+        writeResultInFile("src/main/resources/Task8/output.txt");
     }
-  }
 
+    private static void readDataFromFile(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filename));
+        verticesCount = Integer.valueOf(scanner.nextLine());
+
+        graph = new Vertex[verticesCount];
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new Vertex(i);
+        }
+
+        edgesCount = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < edgesCount; i++) {
+            final String[] splitted = scanner.nextLine().split("\\s");
+            Integer inputVertex = Integer.valueOf(splitted[0]);
+            Integer outputVertex = Integer.valueOf(splitted[1]);
+
+            graph[inputVertex].getOutVertices().put(graph[outputVertex], 1);
+            graph[outputVertex].getOutVertices().put(graph[inputVertex], 1);
+        }
+
+        scanner.close();
+    }
+
+    private static Vertex findVertexWithMinDistance() {
+        Integer minDistance = Integer.MAX_VALUE;
+        Vertex result = null;
+
+        for (int i = 0; i < verticesCount; i++) {
+            Vertex currentVertex = graph[i];
+            if (!currentVertex.isVisited() && currentVertex.getDistance() <= minDistance) {
+                minDistance = currentVertex.getDistance();
+                result = currentVertex;
+            }
+        }
+
+        return result;
+    }
+
+    private static void relaxDistances(Vertex u) {
+        u.setVisited(true);
+
+        for (Vertex v : u.getOutVertices().keySet()) {
+            Integer alt = u.getDistance() + Optional.of(u.getOutVertices().get(v)).orElse(0);
+            if (alt < v.getDistance()) {
+                v.setDistance(alt);
+            }
+        }
+    }
+
+    private static void writeResultInFile(String filename) throws IOException {
+        try (PrintWriter out = new PrintWriter(filename)) {
+            for (int i = 1; i < graph.length; i++) {
+                Integer distance = graph[i].getDistance();
+
+                if (distance == Integer.MAX_VALUE) {
+                    out.println("-1");
+                } else {
+                    out.println(distance);
+                }
+            }
+        }
+    }
+}
+
+@Data
+@ToString(exclude = {"outVertices"})
+@RequiredArgsConstructor()
+@EqualsAndHashCode(exclude = {"outVertices", "visited", "distance"})
+class Vertex {
+
+    @NonNull
+    private int index;
+    private Map<Vertex, Integer> outVertices = new HashMap<>();
+    private boolean visited = false;
+    private Integer distance = Integer.MAX_VALUE;
 }
